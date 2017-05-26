8a902700b336f9e8a98984c31c54843716e9f297
JamesQuillin <jamesquillin1@gmail.com>
2017-05-25 19:19:02 -0700

    Implement findNRooks solution recursively
    Also with not n to the n to the n time complexity

--- src/solvers.js
+++ src/solvers.js
@@ -14,107 +14,130 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-window.decimalToMatrixHelper = function(n, decimalIndex) {
-  // This function takes in a decimal number and
-  // produces an n by n matrix, with leading 0s if necessary,
-  // representing the binary representation of that decimal number.
+// window.decimalToMatrixHelper = function(n, decimalIndex) {
+//   // This function takes in a decimal number and
+//   // produces an n by n matrix, with leading 0s if necessary,
+//   // representing the binary representation of that decimal number.
 
-  /*
-                  [0, 0, 0]  (for a 3 by 3 matrix)
-  8   >  1000  >  [0, 0, 1]
-                  [0, 0, 0]
-  */
+//   /*
+//                   [0, 0, 0]  (for a 3 by 3 matrix)
+//   8   >  1000  >  [0, 0, 1]
+//                   [0, 0, 0]
+//   */
 
-  var binaryMatrix = []; // our matrix to hold the result
-  var decToBinResult = [];
+//   var binaryMatrix = []; // our matrix to hold the result
+//   var decToBinResult = [];
 
-  var buildBinaryNumber = function(num) {
-    if (num > 1) {
-      // console.log('unshift > ', decToBinResult.unshift(num % 2));
-      decToBinResult.unshift(num % 2);
-      buildBinaryNumber(Math.floor(num / 2));
-    } else {
-      // console.log('unshift > ', decToBinResult.unshift(num));
-      decToBinResult.unshift(num);
-    }
-  };
-  // we now have line20array with a binary representation of our decimal number
-  buildBinaryNumber(decimalIndex);
-  //console.log('decToBinResult > ', decToBinResult);
+//   var buildBinaryNumber = function(num) {
+//     if (num > 1) {
+//       // console.log('unshift > ', decToBinResult.unshift(num % 2));
+//       decToBinResult.unshift(num % 2);
+//       buildBinaryNumber(Math.floor(num / 2));
+//     } else {
+//       // console.log('unshift > ', decToBinResult.unshift(num));
+//       decToBinResult.unshift(num);
+//     }
+//   };
+//   // we now have line20array with a binary representation of our decimal number
+//   buildBinaryNumber(decimalIndex);
+//   //console.log('decToBinResult > ', decToBinResult);
 
-  //Add leading zeroes
-  while (decToBinResult.length < n * n) {
-    decToBinResult.unshift(0);
-  }
+//   //Add leading zeroes
+//   while (decToBinResult.length < n * n) {
+//     decToBinResult.unshift(0);
+//   }
 
-  //Determine number of pieces ('1's);
-  var pieceCnt = 0;
-  for (var i = 0; i < decToBinResult.length; i++) {
-    if (decToBinResult[i] === 1) {
-      pieceCnt++;
-    }
-  }
+//   //Determine number of pieces ('1's);
+//   var pieceCnt = 0;
+//   for (var i = 0; i < decToBinResult.length; i++) {
+//     if (decToBinResult[i] === 1) {
+//       pieceCnt++;
+//     }
+//   }
 
-  //If not exactly n number of '1's, return -1 error value
-  if (pieceCnt !== n) {
-    return -1;
-  }
-
-
-  //console.log('decToBinResult > ', decToBinResult);
-  //var popVal; var onesCntPerRow;
-  for (var row = n-1; row >= 0; row--) {
-    //onesCntPerRow = 0;
-    for (var col = n-1; col >= 0; col--) {
-      //var popVal = decToBinResult.pop();
-      if (binaryMatrix[row] === undefined) {
-        binaryMatrix[row] = [decToBinResult.pop()]; //change to popVal
-      } else {
-        binaryMatrix[row].unshift(decToBinResult.pop()); //change to popVal
-      }
-      //if (popVal === 1) onesCntPerRow++;
-      //if (onesCntPerRow > 1) return -1; -- can immediately return since
-      //any time a row has >1 piece it cannot be a solution
-    }
-  }
+//   //If not exactly n number of '1's, return -1 error value
+//   if (pieceCnt !== n) {
+//     return -1;
+//   }
 
 
+//   //console.log('decToBinResult > ', decToBinResult);
+//   //var popVal; var onesCntPerRow;
+//   for (var row = n-1; row >= 0; row--) {
+//     //onesCntPerRow = 0;
+//     for (var col = n-1; col >= 0; col--) {
+//       //var popVal = decToBinResult.pop();
+//       if (binaryMatrix[row] === undefined) {
+//         binaryMatrix[row] = [decToBinResult.pop()]; //change to popVal
+//       } else {
+//         binaryMatrix[row].unshift(decToBinResult.pop()); //change to popVal
+//       }
+//       //if (popVal === 1) onesCntPerRow++;
+//       //if (onesCntPerRow > 1) return -1; -- can immediately return since
+//       //any time a row has >1 piece it cannot be a solution
+//     }
+//   }
 
-  //console.log('binary matrix: ', binaryMatrix);
 
-  return binaryMatrix;
-};
+
+//   //console.log('binary matrix: ', binaryMatrix);
+
+//   return binaryMatrix;
+// };
 
 
 /*
   hasRowConflictAt
   hasAnyRowConflicts
+
   hasColConflictAt
   hasAnyColConflicts
+
+  hasAnyMajorDiagonalConflicts
+  hasAnyMinorDiagonalConflicts
 */
 
 window.findNRooksSolution = function(n) {
-  var currentMatrix;
-  var solution; // = new Board({n: n});
-  //iterate over all possible game boards from 2(exp n) - 1 to 2(exp n*n) - 1
-  var start = (2 ** n) - 1; //Math.pow(7, 2); // > 49
-  var end = (2 ** (n*n)) - 1;
-  for (var i = start; i <= end; i++) {
-    //for each iteration, check using relevant helper functions
-    //if decToMatrixHelper(n,i) !== -1
-    if (decimalToMatrixHelper(n,i) !== -1) {
-      currentMatrix = decimalToMatrixHelper(n,i);  // given 1,1  => [ [1] ]
-      solution = new Board(currentMatrix);
-      console.log('solution board = ', solution);
-      //hasAnyRowConflicts(decimalToMatrixHelper(n, i));
-      //hasAnyColumnConflicts(decimalToMatrixHelper(n, i));
-      if (!solution.hasAnyRowConflicts() && !solution.hasAnyColConflicts()) {
-        //if both return false, return currentMatrix, as it is a valid solution
-        console.log('Single solution for ' + n + ' rooks:', JSON.stringify(currentMatrix));
-        return currentMatrix;
+  var solution = new Board({n: n});
+  console.log('solition > ', solution);  /// [ [1] ]
+
+  var countOfPieces = 0;
+  var firstSolution;
+
+  var inner = function() {
+    if (firstSolution !== undefined) {
+      return firstSolution;
+    } else {
+      for (var i = 0; i < n; i++) {
+        solution.togglePiece(countOfPieces, i);
+        countOfPieces++;
+        console.log('count of pieces: ', countOfPieces);
+
+
+        // we are using countOfPieces as our row and i as our column
+        if (!solution.hasColConflictAt(i) && !solution.hasRowConflictAt(countOfPieces - 1)) {
+          if (countOfPieces === n) {
+            // console.log('line 117 solution rows ------------------: ', solution.rows());
+            firstSolution = solution.rows();
+            return firstSolution;
+          } else {
+            inner();
+          }
+        }
+        if (firstSolution !== undefined) {
+          return firstSolution;
+        } else {
+          countOfPieces--;
+          solution.togglePiece(countOfPieces, i);
+        }
       }
     }
-  }
+  };
+  
+  inner();
+  console.log('expected matrix? ', firstSolution);
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(firstSolution));
+  return firstSolution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
